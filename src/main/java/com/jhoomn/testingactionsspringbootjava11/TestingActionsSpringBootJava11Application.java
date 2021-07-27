2ba634b70abc577cc3cfd6eea0a7684237b5f859
Jhon Camilo Baron Berdugo <jbaron@mutualser.org>
2021-07-27 06:52:04 -0500

    first commit

---
+++ src/main/java/com/jhoomn/testingactionsspringbootjava11/TestingActionsSpringBootJava11Application.java
@@ -0,0 +1,13 @@
+package com.jhoomn.testingactionsspringbootjava11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingActionsSpringBootJava11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingActionsSpringBootJava11Application.class, args);
+	}
+
+}
